589e4e4e982d93eac47d89ddf3cea29e7742ea37
serkanok1983 <serkanok1983@yahoo.com>
2025-01-26 23:50:39 +0300

    .env & note.js

--- index.js
+++ index.js
@@ -1,113 +1,87 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const Note = require("./models/note");
 
-app.use(cors())
-app.use(express.json())
-app.use(express.static('dist'))
+app.use(cors());
+app.use(express.json());
+app.use(express.static("dist"));
 
 const requestLogger = (req, res, next) => {
-    console.log('Method:', req.method)
-    console.log('Path:', req.path)
-    console.log('Body:', req.body)
-    console.log('---')
-    next()
-}
+  console.log("Method:", req.method);
+  console.log("Path:", req.path);
+  console.log("Body:", req.body);
+  console.log("---");
+  next();
+};
 
-app.use(requestLogger)
+app.use(requestLogger);
 
 let notes = [
-    {
-        id: "1",
-        content: "HTML is easy",
-        important: true
-    },
-    {
-        id: "2",
-        content: "Browser can execute only Javascript",
-        important: false
-    },
-    {
-        id: "3",
-        content: "GET and POST are the most important methods of HTTP protocol",
-        important: true
-    }
-]
+  {
+    id: "1",
+    content: "HTML is easy",
+    important: true,
+  },
+  {
+    id: "2",
+    content: "Browser can execute only Javascript",
+    important: false,
+  },
+  {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
+];
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
-})
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
 
-app.get('/api/notes', (req, res) => {
-    res.json(notes)
-})
+app.get("/api/notes", (req, res) => {
+  Note.find({}).then((result) => {
+    res.json(result);
+  });
+});
 
-app.get('/api/notes/:id', (req, res) => {
-    const id = req.params.id
-    const note = notes.find(note => note.id === id)
-    if (note) {
-        res.json(note)
-    } else {
-        res.status(404).end()
-    }
-})
+app.get("/api/notes/:id", (req, res) => {
+  Note.findById(req.params.id).then((note) => {
+    res.json(note);
+  });
+});
 
-app.delete('/api/notes/:id', (req, res) => {
-    const id = req.params.id
-    notes = notes.filter(note => note.id !== id)
-    res.status(204).end()
-})
+app.delete("/api/notes/:id", (req, res) => {
+  const id = req.params.id;
+  notes = notes.filter((note) => note.id !== id);
+  res.status(204).end();
+});
 
-const generateId = () => {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map(n => Number(n.id)))
-        : 0
-    return String(maxId + 1)
-}
+app.post("/api/notes", (req, res) => {
+  const body = req.body;
 
-app.post('/api/notes', (req, res) => {
-    const body = req.body
+  if (body.content === undefined) {
+    return res.status(400).json({ error: "content missing" });
+  }
 
-    if (!body.content) {
-        return res.status(400).json({
-            error: 'content missing'
-        })
-    }
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  });
 
-    const note = {
-        content: body.content,
-        important: Boolean(body.important) || false,
-        id: generateId(),
-    }
-
-    notes = notes.concat(note)
-
-    res.json(note)
-})
+  note.save().then((savedNote) => {
+    res.json(savedNote);
+  });
+});
 
 const unknownEndpoint = (req, res) => {
-    res.status(404).send({error: 'unknown endpoint'})
-}
+  res.status(404).send({ error: "unknown endpoint" });
+};
 
-app.use(unknownEndpoint)
+app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
-
-const mongoose = require('mongoose')
-
-const password = process.argv[2]
-
-const url = `mongodb+srv://serkanok1983:${password}@phonebook.7rdo7.mongodb.net/?retryWrites=true&w=majority&appName=phonebook`
-
-mongoose.set('strictQuery', false)
-mongoose.connect(url)
-
-const noteSchema = new mongoose.Schema({
-    content: String,
-    important: Boolean,
-})
-
-const Note = mongoose.model('Note', noteSchema)
+  console.log(`Server running on port ${PORT}`);
+});
