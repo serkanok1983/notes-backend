ea6e5a7d3540fbafdb19226f0c9967de4096557b
serkanok1983 <serkanok1983@yahoo.com>
2025-01-28 14:43:32 +0300

    delete functionality

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ require("dotenv").config();
 const Note = require("./models/note");
 
 app.use(cors());
-app.use(express.json());
 app.use(express.static("dist"));
+app.use(express.json());
 
 const requestLogger = (req, res, next) => {
   console.log("Method:", req.method);
@@ -15,8 +15,6 @@ const requestLogger = (req, res, next) => {
   console.log("---");
   next();
 };
-
-app.use(requestLogger);
 
 let notes = [
   {
@@ -46,16 +44,44 @@ app.get("/api/notes", (req, res) => {
   });
 });
 
-app.get("/api/notes/:id", (req, res) => {
-  Note.findById(req.params.id).then((note) => {
-    res.json(note);
-  });
+app.get("/api/notes/:id", (req, res, next) => {
+  Note.findById(req.params.id)
+    .then((note) => {
+      if (note) {
+        res.json(note);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/notes/:id", (req, res) => {
-  const id = req.params.id;
-  notes = notes.filter((note) => note.id !== id);
-  res.status(204).end();
+// app.delete("/api/notes/:id", (req, res) => {
+//   const id = req.params.id;
+//   notes = notes.filter((note) => note.id !== id);
+//   res.status(204).end();
+// });
+app.delete("/api/notes/:id", (req, res, next) => {
+  Note.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+app.put("/api/notes/:id", (req, res, next) => {
+  const body = req.body;
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  };
+
+  Note.findByIdAndUpdate(req.params.id, note, { new: true })
+    .then((updatedNote) => {
+      res.json(updatedNote);
+    })
+    .catch((error) => next(error));
 });
 
 app.post("/api/notes", (req, res) => {
@@ -80,6 +106,20 @@ const unknownEndpoint = (req, res) => {
 };
 
 app.use(unknownEndpoint);
+
+app.use(requestLogger);
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return res.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
